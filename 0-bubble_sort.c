e38531c9c544c0c10abcf602aaa365ebd09ccad1
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-24 22:57:32 +0200

    correct position for print

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,8 +30,8 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				swap_func(&array[j], &array[j + 1]);
+				print_array(array,size);
 			}
 		}
-		print_array(array,size);
 	}
 }
