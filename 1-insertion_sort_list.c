5c7442daaef5a01e576711e841eee88abdda22e5
SlaveToPurpose <keziahleroux@gmail.com>
2023-10-25 10:49:33 +0200

    betty eish

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,63 +1,54 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list with insertion
- * @list: Doubly linked list
-*/
+ * swap_nodes - Swap nodes in a list.
+ * @h: A pointer to the head
+ * @n1: A pointer to the first node
+ * @n2: The second node
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
 
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+
+	if ((*n1)->prev != NULL)
+	{
+		(*n1)->prev->next = n2;
+	}
+	else
+	{
+		*h = n2;
+	}
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *node, *head;
+	listint_t *count, *insert, *temp;
 
-	if (list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
 
-	if (*list == NULL)
+	for (count = (*list)->next; count != NULL; count = temp)
 	{
-		return;
-	}
-
-	head = *list;
-
-	while (head)
-	{
-		if (head->next && (head->n > head->next->n))
+		temp = count->next;
+		insert = count->prev;
+		while (insert != NULL && count->n < insert->n)
 		{
-			temp = head;
-			head->next->prev = head->prev;
-
-			if (head->prev)
-			{
-				head->prev->next = head->next;
-			}
-			else
-			{
-				*list = head->next;
-			}
-
-			node = head->next;
-			temp->prev = NULL;
-			temp->next = NULL;
-
-			if (node->next)
-			{
-				temp->next = node->next;
-				node->next->prev = temp;
-				node->next = temp;
-				temp->prev = node;
-			}
-			else
-			{
-				node->next = temp;
-				temp->prev = node;
-			}
-
-			print_list(*list);
-			head = *list;
-			continue;
+			swap_nodes(list, &insert, count);
+			print_list((const listint_t *)*list);
 		}
-		head = head->next;
 	}
 }
--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ typedef struct listint_s
 
 
 void bubble_sort(int *array, size_t size);
-void swap_func(int *i, int*j);
+void swap_func(int *i, int *j);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
@@ -29,5 +29,6 @@ void swap(int *a, int *b);
 size_t lomuto_partition(int *array, size_t low, size_t high);
 void quick_sort_recursive(int *array, size_t low, size_t high);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif /* SORT_H */
